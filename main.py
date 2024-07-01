590275433f9bfccf3e14fec35ab2018a35dfa0c6
Anton Salamatin <91375336+anton1443@users.noreply.github.com>
2024-07-01 18:54:54 +0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import string
 
 symbols = string.ascii_letters + string.digits + string.punctuation
-password = str()
+password = ''
 for i in range(21):
     password += random.choice(symbols)
 print(password)
